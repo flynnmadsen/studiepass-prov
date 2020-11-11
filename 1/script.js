15f85a9be2047488c53b0df1b32d307b5904e668
Flynn Madsen <flynnisobelhugym.madsen@skola.botkyrka.se>
2020-11-11 13:27:40 +0100

    uppgift 1

--- 1/script.js
+++ 1/script.js
@@ -1,8 +1,11 @@
 
 // 1. Deklarera en variabel a och tilldela den värdet 5. Byt sedan värdet på a till 8
-
+let a = 5
+a = 8 
+console.log(a)
 // 2. Deklarera en konstant variabel tilldela den värdet av ditt förnamn
-
+const name = "Flynn"
+console.log(name)
 // 3. 
 // a) Hur många argument tar den här funktionen? SVAR: ____
 // b) Vilken datatyp returnerar den? SVAR: _____
