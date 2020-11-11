1c1fe68f4481f780b7e17e7ba3f85f884537b628
Flynn Madsen <flynnisobelhugym.madsen@skola.botkyrka.se>
2020-11-11 14:39:29 +0100

    uppgift 4

--- 4/script.js
+++ 4/script.js
@@ -1,12 +1,14 @@
 // 1. Skapa en variabel som innehåller en array med fyra strängar, t.ex. fyra namn på personer.
-
-// 2. Logga ut arrayen.
-
-// 3. Byt ut det andra elementet till något annat.
-
-// 4. Logga ut arrayen igen.
-
-// 5. Logga ut det sista elementet i arrayen.
-
 let arrraaayyy = ["Jonathan", "Einar", "Hampus", "Liam"]
+// 2. Logga ut arrayen.
 console.log(arrraaayyy)
+// 2b. logga ut andra elementet
+console.log(arrraaayyy[2])
+// 3. Byt ut det andra elementet till något annat.
+arrraaayyy[1] = "Flynn"
+// 4. Logga ut arrayen igen.
+console.log(arrraaayyy)
+// 5. Logga ut det sista elementet i arrayen.
+console.log(arrraaayyy[3])
+
+
